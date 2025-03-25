a0251377979c1df27105471de983409456f21742
TinkGu <tlwrite@126.com>
2025-03-25 12:23:12 +0800

    chore: fix public path error

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'development' ? '/' : '/game-data-editor/',
   plugins: [react()],
   root: path.resolve(__dirname),
   resolve: {
