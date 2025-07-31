810fa2ea4674606bd1fa3935e65ee643773cc78c
TinkGu <tlwrite@126.com>
2025-07-31 16:44:49 +0800

    fix: 临时去除 sw

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,4 +2,10 @@ import { render } from 'react-dom';
 import Routes from './routes';
 import './index.css';
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.ready.then((registration) => {
+    registration.unregister();
+  });
+}
+
 render(<Routes />, document.getElementById('root'));
